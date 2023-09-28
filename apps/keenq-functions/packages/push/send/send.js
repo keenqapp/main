d6c8507b984c31afcbe830c15229f38cbf5813d9
boris <veligorya.boris@gmail.com>
2023-09-28 10:12:55 +0300

    fix(push/send): fix validation

--- apps/keenq-functions/packages/push/send/send.js
+++ apps/keenq-functions/packages/push/send/send.js
@@ -1,5 +1,5 @@
 import { object, string, mixed } from 'yup'
-// import webpush from 'web-push'
+import webpush from 'web-push'
 
 import { getDb, getCreds, ensureCreds, success, error, validate } from './shared.js'
 
@@ -9,46 +9,46 @@ const schema = object({
 	payload: mixed()
 })
 
-// const config = {
-// 	client: 'pg',
-// 	connection: {
-// 		connectionString: process.env.DB_CONNECTION_STRING,
-// 		ssl: { rejectUnauthorized: false },
-// 	},
-// 	pool: { min: 0, max: 2 }
-// }
-//
-// const vapidPublicKey = process.env.VAPID_PUBLIC_KEY
-// const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY
-//
-// function getPushes() {
-// 	webpush.setVapidDetails('https://keenq.app', vapidPublicKey, vapidPrivateKey)
-// 	return webpush.sendNotification
-// }
-//
-// async function getMember(id, db) {
-// 	return db.table('members').select().where('id', id).first()
-// }
-//
-// async function send(payload, endpoint) {
-// 	return
-// }
+const config = {
+	client: 'pg',
+	connection: {
+		connectionString: process.env.DB_CONNECTION_STRING,
+		ssl: { rejectUnauthorized: false },
+	},
+	pool: { min: 0, max: 2 }
+}
+
+const vapidPublicKey = process.env.VAPID_PUBLIC_KEY
+const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY
+
+function getPushes() {
+	webpush.setVapidDetails('https://keenq.app', vapidPublicKey, vapidPrivateKey)
+	return webpush.sendNotification
+}
+
+async function getMember(id, db) {
+	return db.table('members').select().where('id', id).first()
+}
+
+async function send(payload, endpoint) {
+	return
+}
 
 export async function main(body) {
 	let db
 	try {
-		const { id, payload } = validate(schema, body)
+		const { id, payload } = validate(body, schema)
 
-		// db = getDb(config)
-		//
-		// const creds = await getCreds(id, db)
-		// await ensureCreds(creds)
-		//
-		// const member = await getMember(id, db)
+		db = getDb(config)
 
-		// const pushes = getPushes()
+		const creds = await getCreds(id, db)
+		await ensureCreds(creds)
 
-		return success({ id })
+		const member = await getMember(id, db)
+
+		const pushes = getPushes()
+
+		return success({ member })
 	}
 	catch(e) {
 		return error(e)
