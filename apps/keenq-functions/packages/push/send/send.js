00677f5b9cedfab510022bb2e17def67d8ef6929
boris <veligorya.boris@gmail.com>
2023-09-28 09:49:53 +0300

    chore: test

--- apps/keenq-functions/packages/push/send/send.js
+++ apps/keenq-functions/packages/push/send/send.js
@@ -1,41 +1,38 @@
-import { object, string } from 'yup'
-import webpush from 'web-push'
+import { object, string, mixed } from 'yup'
+// import webpush from 'web-push'
 
 import { getDb, getCreds, ensureCreds, success, error, validate } from './shared.js'
 
 
 const schema = object({
 	id: string().required(),
-	payload: object({
-		title: string().required(),
-		body: string().required(),
-	})
+	payload: mixed()
 })
 
-const config = {
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DB_CONNECTION_STRING,
-		ssl: { rejectUnauthorized: false },
-	},
-	pool: { min: 0, max: 2 }
-}
-
-const vapidPublicKey = process.env.VAPID_PUBLIC_KEY
-const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY
-
-function getPushes() {
-	webpush.setVapidDetails('https://keenq.app', vapidPublicKey, vapidPrivateKey)
-	return webpush.sendNotification
-}
-
-async function getMember(id, db) {
-	return db.table('members').select().where('id', id).first()
-}
-
-async function send(payload, endpoint) {
-	return
-}
+// const config = {
+// 	client: 'pg',
+// 	connection: {
+// 		connectionString: process.env.DB_CONNECTION_STRING,
+// 		ssl: { rejectUnauthorized: false },
+// 	},
+// 	pool: { min: 0, max: 2 }
+// }
+//
+// const vapidPublicKey = process.env.VAPID_PUBLIC_KEY
+// const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY
+//
+// function getPushes() {
+// 	webpush.setVapidDetails('https://keenq.app', vapidPublicKey, vapidPrivateKey)
+// 	return webpush.sendNotification
+// }
+//
+// async function getMember(id, db) {
+// 	return db.table('members').select().where('id', id).first()
+// }
+//
+// async function send(payload, endpoint) {
+// 	return
+// }
 
 export async function main(body) {
 	let db
