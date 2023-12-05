0d1dca16211442c0646244a9e28392d41d6e0a00
Boris Veligorya <veligorya.boris@gmail.com>
2023-12-05 21:41:58 +0400

    add expired cache

--- apps/keenq-api/resourses/utils/rates.js
+++ apps/keenq-api/resourses/utils/rates.js
@@ -1,14 +1,20 @@
 import axios from 'axios'
 import { success, error } from '../../shared.js'
 
-let cache = null
+let cache = {
+	date: null,
+	rates: null
+}
 
 async function getRates() {
-	if (cache) return cache
+	if (cache.rates && cache.date >= Date.now()) return cache.rates
 	let url = 'http://data.fixer.io/api/latest?access_key=44f13e26047ce6151339cc637fc30fb7'
 	const result = (await axios.get(url))?.data?.rates || null
-	cache = result
-	return cache
+	cache = {
+		date: (new Date()).setDate((new Date).getDate() + 1),
+		rates: result
+	}
+	return cache.rates
 }
 
 export default async function rates() {
